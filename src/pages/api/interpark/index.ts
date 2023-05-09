364d7c9c01b00a8bc4673e4e6f4b09b178be4fa8
ungjinPark <ungjin312@kakao.com>
2023-05-09 16:14:02 +0900

    update

--- src/pages/api/interpark/index.ts
+++ src/pages/api/interpark/index.ts
@@ -9,6 +9,7 @@ export type Review = {
   views?:string
   title?:string
   content?:string
+  branch?:string
 }
 
 const viewport = {width:1920,height:1080};
@@ -24,16 +25,19 @@ async function getReview(collecter:Browser,url:string){
     return items.map(_=>{
       const infoNode = _.querySelectorAll(".rightSide .bbsItemInfoList");
       const name=infoNode[0].querySelector(".name")?.textContent ?? "알 수 없음",
-      star = document.querySelector('[data-star]')?.getAttribute('data-star') ?? "알수 없음",
+      star=_.querySelector('[data-star]')?.getAttribute('data-star') ?? "알수 없음",
       date=infoNode[1].textContent ?? "알 수 없음",
       views=infoNode[2].textContent ?? "알 수 없음",
       like=infoNode[3].textContent ?? "알 수 없음";
       const title = _.querySelector(".bbsTitle")?.textContent ?? "알 수 없음";
       const content = _.querySelector(".bbsText")?.textContent ?? "알 수 없음";
+      const branch = document.title.match(/북촌|동탄/);
+      
       return {
         name,
         star,
         date,
+        branch: branch ? branch[0] : "알 수 없음",
         views,
         like:like.replace("공감하기",""),
         title,
@@ -42,21 +46,41 @@ async function getReview(collecter:Browser,url:string){
     })
   })
   await page.close();
-  return reviewList;
+  return await reviewList;
 }
 export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse<{
-    list:Review[]
+    list:Review[][]
+    length?:number
   }>
 ) {
   const collecter = await puppeteer.launch({headless:"new",defaultViewport:viewport});
   const page = await collecter.newPage();
   await page.goto("https://tickets.interpark.com/search?keyword=DIALOGUE%20IN%20THE%20DARK");
-  await page.setRequestInterception(true);
-  await page.waitForSelector("ul.itemList")
-  const urls = await page.$$eval("ul.itemList li .itemName>a",(a)=>a.map(_=>_.href));
-  const lists = await (await PromisePool.for([...urls.map(async url=>await getReview(collecter,url))]).process((_)=>_)).results;
-  const list = await lists.flat();
-  res.status(200).json({list:list})
+  await page.waitForSelector("ul.itemList");
+  const items = await (await page.$$eval("#ticketContent .itemList>li",(li)=>{
+    const list = li.map((_)=>{  
+        return {
+          url:_.querySelector(".itemName>a")?.getAttribute('href')!,
+          onSale:_.querySelector(".tag.onSale")?.textContent == "판매중",
+      };
+    })
+    const filtered = list.filter(_=>_.onSale);
+    const latest = list.slice(0,3);
+    if ( filtered.length < 2 ) {
+      return latest;
+    }
+    return filtered;
+  }))
+  const reviews = await (await (await PromisePool.for(items.map(async items=>getReview(collecter,items.url))).process(_=>_))).results;
+  const review = await reviews.map(_=>_.sort((a,b)=>{
+    const aDate=new Date(a.date?.replace(".","-") ?? Date.now())
+    const bDate=new Date(b.date?.replace(".","-") ?? Date.now())
+    if(aDate>bDate) {
+      return -1;
+    }
+    return 0;
+  }));
+  res.status(200).json({list:review,length:reviews.length})
 }
--- src/pages/interpark.tsx
+++ src/pages/interpark.tsx
@@ -1,36 +1,70 @@
 import MainLayout from "@/layout";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Review } from "./api/interpark";
 
 export default function Interpark(){
-    const [items,updateItems] = useState<Review[]>([]);
+    const [items,updateItems] = useState<Review[][]>([]);
     const [isLoaded,setIsLoaded] = useState<boolean>(false);
+    const ref = useRef<HTMLDivElement|null>(null);
     useEffect(()=>{
         (async()=>{
             const response = await axios.get("/api/interpark")
-            const {data} = response;
-            return data.list;
+            const {data} = await response;
+            return await data.list;
         })().then(r=>{
-            updateItems(r as Review[])
+            updateItems(r as Review[][])
             setIsLoaded(true);
+            ref.current?.focus();
         });
+        return;
     },[])
     return (
         <MainLayout title="어둠속의 대화 리뷰 - 인터파크">
-        <div style={{display: !isLoaded ? "block" : "none"}}>리뷰를 로드하는 중...</div>
-          {items.map((review,idx)=>{
-            return (
-                <>
-                    <article key={idx}>
-                        <h2>{review.title}</h2>
-                        <div className="info">
-                            <p className="username">{review.name}</p> | <p className="username">{review.date}</p>
-                        </div>
-                    </article>
-                </>
-            )
-          })}
+        <div style={{display: !isLoaded ? "block" : "none"}} tabIndex={-1} ref={(r)=>{
+            if(isLoaded){
+                setTimeout(()=>{
+                    r?.focus();
+                },100);
+            }
+        }}>리뷰를 로드하는 중...</div>
+        <div className="reviews" tabIndex={-1} ref={isLoaded ? ref : null}>
+            {items.map((sect,idx)=>{
+                return (
+                    <section key={idx} id={sect[0].branch == "북촌" ? "bukchon" : "dongtan"}>
+                        <h2 style={{textAlign:"center"}}>{sect[0].branch}</h2>
+                        {sect.map((review,idx)=>{
+                            return (
+                            <article key={idx}>
+                                <h3>[{review.branch}]{review.title}</h3>
+                                <div className="info">
+                                    <p className="username">{review.name}|{review.date}</p>
+                                    <p className="stars" style={{userSelect:"none"}}>
+                                    <span aria-hidden={true}>
+                                        별점:
+                                    </span>
+                                    <span className="star-graphic" style={{
+                                        "--progress":`${(()=>{
+                                            const percent = (Number(review.star))/5*100;
+                                            return `${percent}%`;
+                                        })()}`,
+                                    } as React.CSSProperties} aria-valuemin={0} aria-valuenow={Number(review.star)} aria-valuemax={5} role={"meta"}
+                                    aria-label={"별점"}>★★★★★</span>
+                                    <span aria-hidden={true}>({Number(review.star).toFixed(1)})</span>
+                                    </p>
+                                </div>
+                                <div className="body">
+                                    <p className="content">
+                                        {review.content}
+                                    </p>
+                                </div>
+                            </article>
+                        )})}
+                        
+                    </section>
+                );
+            })}
+        </div>
         </MainLayout>
       )
 }
